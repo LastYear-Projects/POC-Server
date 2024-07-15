0db65ca6408e3cb034f1718646e588dbfb6c18a9
YuvalOfek1 <yuvalofek97@gmail.com>
2024-07-15 17:28:14 +0300

    creditCardId in benefit is mandatory

--- src/schemas/benefitSchema.ts
+++ src/schemas/benefitSchema.ts
@@ -4,7 +4,7 @@ import objectIdSchema from "./objectIdSchema";
 
 const benefitSchema = z.object({
   businessId: objectIdSchema.optional(),
-  creditCardId: objectIdSchema.optional(),
+  creditCardId: objectIdSchema,
   discountType: discountType,
   valueType: valueType,
   value: z.number(),
