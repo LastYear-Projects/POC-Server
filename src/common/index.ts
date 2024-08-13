dcc08f5ca65ad0ffbd99fd20b55bdbf528ba3ab9
Idan Asayag <idanasayag0@gmail.com>
2024-08-13 15:27:44 +0300

    after refresh token this line will changes.

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,5 +1,6 @@
-import jwt from "jsonwebtoken"
+import jwt from 'jsonwebtoken';
 export const generateToken = (payload: any) => {
-    return jwt.sign(payload, process.env.JWT_SECRET_KEY, {expiresIn: "10m"});
-}
-
+  return jwt.sign(payload, process.env.JWT_SECRET_KEY, {
+    expiresIn: '99999999m',
+  });
+};
