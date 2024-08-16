64a02956a5101a588e74db4eb85e3abf37b76e09
DanDavidovich <davidovich.dan@gmail.com>
2024-08-16 11:11:19 +0300

    scraping fix

--- src/scraping/index.js
+++ src/scraping/index.js
@@ -11,11 +11,9 @@ function containsCashbackKeywords(text) {
 // Function to construct absolute URL from base URL and relative URL
 function resolveUrl(baseUrl, relativeUrl) {
   if (!relativeUrl) return null;
-
   // Remove any trailing slashes from base URL and leading slashes from relative URL
   baseUrl = baseUrl.replace(/\/+$/, '');
   relativeUrl = relativeUrl.replace(/^\/+/, '');
-
   // Combine base URL and relative URL
   return `${baseUrl}/${relativeUrl}`;
 }
@@ -46,6 +44,17 @@ function reOrderBidiText(text) {
         return segment;
       })
       .join(' ');
+}
+
+function getFirstWord(text) {
+  // Trim any extra spaces
+  text = text.trim();
+
+  // Define the regular expression to match common delimiters
+  const delimiterPattern = /[ \-_.,;:|!]/;
+
+  // Split the text by the defined delimiters and return the first part
+  return text.split(delimiterPattern)[0].trim();
 }
 
 // Function to scrape data
@@ -118,22 +127,10 @@ async function scrapeWebsiteIsracrd(url) {
       console.error(`Error scraping the website ${url}:`, error);
     }
   }
-
-
-  function getFirstWord(text) {
-    // Trim any extra spaces
-    text = text.trim();
-
-    // Define the regular expression to match common delimiters
-    const delimiterPattern = /[ \-_.,;:|!]/;
-
-    // Split the text by the defined delimiters and return the first part
-    return text.split(delimiterPattern)[0].trim();
-  }
-
 }
 
 async function scrapeWebsiteHever(url) {
+  const creditCardId = '66bb7766995330bf77f4ccb2';
   try {
     const { data } = await axios.get(url);
     const $ = cheerio.load(data);
@@ -142,79 +139,56 @@ async function scrapeWebsiteHever(url) {
     $('.retailer_preview ').each((index, element) => {
       const title = $(element).find('.tete.ellipsis').text().trim();
       const subTitle = $(element).find('.slider h4 ').text().trim();
-      // Extract image URL from src attribute
       const imageUrl = $(element).find('.preview_logo').attr('data-src');
-      // Convert relative URL to absolute URL
       const backgroundImage = resolveUrl(baseUrl, imageUrl);
 
-      // if (containsCashbackKeywords(title) || containsCashbackKeywords(subTitle)) {
-      //   cashbackItems.push({
-      //     businessName: reOrderBidiText(title),
-      //     creditCardId: creditCardId,
-      //     discountType: 'cashback',
-      //     valueType: benefit.subTitle.includes('%') ? 'percentage' : 'number',
-      //     value: extractNumber(benefit.subTitle),
-      //     businessImage: benefit.backgroundImage,
-      //     backgroundImage,
-      //   });
-      // }
+      if (containsCashbackKeywords(title) || containsCashbackKeywords(subTitle)) {
+        cashbackItems.push({
+          businessName: reOrderBidiText(title),
+          businessSubTitle: reOrderBidiText(subTitle),
+          creditCardId: creditCardId,
+          discountType: 'cashback',
+          valueType: subTitle.includes('%') ? 'percentage' : 'number',
+          value: extractNumber(subTitle),
+          businessImage: backgroundImage,
+        });
+      }
     });
 
-    // Step 4: Fetch details for each cashback item
-    for (const item of cashbackItems) {
-      if (item.detailUrl) {
-        try {
-          const detailData = await axios.get(item.detailUrl);
-          const $detail = cheerio.load(detailData.data);
+    console.log(cashbackItems);
 
-          const cashbackTitle = $detail('.cashBack-title').text().trim();
-          const cashbackDescription = $detail('.cashBack-description').text().trim();
-
-          item.cashbackDetails = {
-            title: reOrderBidiText(cashbackTitle),
-            description: reOrderBidiText(cashbackDescription),
-          };
-        } catch (detailError) {
-          console.error(`Error fetching details for item ${item.title}:`, detailError);
-        }
-      }
-    }
-
-    pipeToSwipeAdvisor(cashbackItems,'66bb7766995330bf77f4ccb2');
   } catch (error) {
     console.error(`Error scraping the website ${url}:`, error);
   }
 
 
-  function getFirstWord(text) {
-    // Trim any extra spaces
-    text = text.trim();
 
-    // Define the regular expression to match common delimiters
-    const delimiterPattern = /[ \-_.,;:|!]/;
-
-    // Split the text by the defined delimiters and return the first part
-    return text.split(delimiterPattern)[0].trim();
-  }
 
 }
 
 
-const pipeToSwipeAdvisor = (data, creditCardId) => {
-  const newBenefits = data.map((benefit) => ({
-    businessName: reOrderBidiText(benefit.title),
-    businessSubTitle: reOrderBidiText(benefit.subTitle),
-    creditCardId: creditCardId,
-    discountType: 'cashback',
-    valueType: benefit.subTitle.includes('%') ? 'percentage' : 'number',
-    value: extractNumber(benefit.subTitle),
-    businessImage: benefit.backgroundImage,
-  }));
-};
+// const pipeToSwipeAdvisor = (data, creditCardId) => {
+//   return data.map((benefit) => {
+//     const newBenefit = {
+//       businessName: reOrderBidiText(benefit.title),
+//       businessSubTitle: reOrderBidiText(benefit.subTitle),
+//       creditCardId: creditCardId,
+//       discountType: 'cashback',
+//       valueType: benefit.subTitle.includes('%') ? 'percentage' : 'number',
+//       value: extractNumber(benefit.subTitle),
+//       businessImage: benefit.backgroundImage,
+//     };
+//
+//     // Print each newBenefit object
+//     console.log(newBenefit);
+//
+//     return newBenefit;
+//   });
+// };
 
 
-scrapeWebsiteIsracrd('https://benefits.isracard.co.il/parentcategories/online-benefits/');
-scrapeWebsiteHever('https://www.cashback-hvr.co.il/all-shops?mid=269341&sig=d3a57daf4636f8b19e77834757bd3b1b');
+// scrapeWebsiteIsracrd('https://benefits.isracard.co.il/parentcategories/online-benefits/');
+scrapeWebsiteHever('https://www.cashback-hvr.co.il/category/travel?mid=269341&sig=f8d7e3d24bccb99dfcafb5ddd354c202');
 
 
 
